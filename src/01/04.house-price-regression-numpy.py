062572256a1b10773c7df9ec32a1b02e342a1d83
zhipeng.liang@vwg-connect.cn <zhipeng.liang>
2020-05-03 23:40:49 +0800

    完成 线性回归

--- src/01/04.house-price-regression-numpy.py
+++ src/01/04.house-price-regression-numpy.py
@@ -14,6 +14,7 @@ import numpy as np
 X_raw = np.array([2013, 2014, 2015, 2016, 2017], dtype=np.float32)
 y_raw = np.array([12000, 14000, 15000, 16500, 17500], dtype=np.float32)
 
+# 归一化
 X = (X_raw - X_raw.min()) / (X_raw.max() - X_raw.min())
 y = (y_raw - y_raw.min()) / (y_raw.max() - y_raw.min())
 
